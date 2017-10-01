73df90093c07aa4d1f27e91f8c7458b5fa008bdb
tarakc02 <tarakc02@github.com>
2017-10-01 15:24:42 -0700

    distance metrics should return integers

--- R/helper-functions.R
+++ R/helper-functions.R
@@ -3,7 +3,7 @@ is_even <- function(x) !is_odd(x)
 
 ind_to_coord <- function(pz, ind) {
     n <- dim(pz)[1]
-    c(1, 1) + c((ind - 1) %% n, (ind - 1) %/% n)
+    c(1L, 1L) + c((ind - 1L) %% n, (ind - 1L) %/% n)
 }
 
 coord_to_ind <- function(pz, coord) {
@@ -13,7 +13,7 @@ coord_to_ind <- function(pz, coord) {
 
 goal_index <- function(pz, num) {
     n <- dim(pz)[1]
-    c(1, 1) +
+    c(1L, 1L) +
         c((num - 1L) %/% n,
           (num - 1L) %% n)
 }
