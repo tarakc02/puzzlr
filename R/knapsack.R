b69fe0e5217ad5896b91b1bc6c4195f819aab4c1
tarakc02 <tarakc02@github.com>
2018-01-29 10:18:17 -0800

    vignette (in progress)

--- R/knapsack.R
+++ R/knapsack.R
@@ -24,9 +24,25 @@ next_item.knapsack <- function(ks) ks$items("top")
 n_items <- function(ks) UseMethod("n_items")
 n_items.knapsack <- function(ks) ks$items("size")
 
-take_items <- function(ks, ids) {
+total_value <- function(ks) UseMethod("total_value")
+total_value.knapsack <- function(ks) ks$value
+
+take <- function(ks) UseMethod("take")
+leave <- function(ks) UseMethod("leave")
+take.knapsack <- function(ks) next_knapsack(ks, take = TRUE)
+leave.knapsack <- function(ks) next_knapsack(ks, take = FALSE)
+
+taken_items <- function(ks) UseMethod("taken_items")
+taken_items.knapsack <- function(ks) {
+    lst <- stack_to_list(ks$taken)
+    lst <- purrr::map(lst, tibble::as_data_frame)
+    dplyr::bind_rows(lst)
+}
+
+take_items <- function(ks, ids) UseMethod("take_items")
+take_items.knapsack <- function(ks, ids) {
     while(ks$items("size") > 0)
-        ks <- if (next_item(ks)$id %in% ids) take(ks) else dont_take(ks)
+        ks <- if (next_item(ks)$id %in% ids) take(ks) else leave(ks)
     ks
 }
 
@@ -47,9 +63,63 @@ print.knapsack <- function(x, ...) {
         its <- its("without_top")
     }
     if (its("size") > 0) cat("   ...")
-    cat("\nTaken Items:", x$taken("size"), "\n")
+    cat("\nTaken items:", x$taken("size"), "\n")
     cat("Total value:", x$value)
     invisible(x)
+}
+
+next_knapsack <- function(kss, take) {
+    if (kss$items("size") <= 0L) return(NULL)
+    current_item <- kss$items("top")
+
+    if (take) return(structure(list(
+        capacity = kss$capacity - current_item$weight,
+        items = kss$items("without_top"),
+        value = kss$value + current_item$value,
+        taken = push(kss$taken, current_item)
+    ), class = "knapsack"))
+
+    structure(list(capacity = kss$capacity,
+                   items = kss$items("without_top"),
+                   value = kss$value,
+                   taken = kss$taken),
+              class = "knapsack")
+}
+
+
+sub_problems <- function(ks) {
+    remaining_items <- ks$items
+    if (remaining_items("size") <= 0L) return(list())
+
+    remaining_capacity <- ks$capacity
+    next_item <- remaining_items("top")
+    if (next_item$weight <= remaining_capacity) return(
+        list(next_knapsack(ks, take = TRUE),
+             next_knapsack(ks, take = FALSE))
+    )
+    list(next_knapsack(ks, take = FALSE))
+}
+
+item_df <- function(ks) {
+    is <- ks$items
+    id <- vector("integer", is("size"))
+    weight <- vector("integer", is("size"))
+    value <- vector("integer", is("size"))
+
+    row <- 1L
+    while (is("size") > 0) {
+        item <- is("top")
+        id[row] <- item$id
+        weight[row] <- item$weight
+        value[row] <- item$value
+        is <- is("without_top")
+        row <- row + 1L
+    }
+
+    structure(
+        data.frame(id = id, weight = weight, value = value),
+        class = c("tbl_df", "tbl", "data.frame")
+    )
 }
 
 # random knapsack stuff from:
--- R/stack.R
+++ R/stack.R
@@ -16,6 +16,17 @@ push <- function(s, new_item) {
                size = sz)
 }
 
+stack_to_list <- function(s) {
+    res <- vector("list", s("size"))
+    i <- 1L
+    while (s("size") > 0) {
+        res[[i]] <- s("top")
+        i <- i + 1L
+        s <- s("without_top")
+    }
+    res
+}
+
 stackify <- function(items, density_order) {
     if (density_order) items <- dplyr::arrange(items, value / weight)
     res <- stack()
@@ -24,58 +35,3 @@ stackify <- function(items, density_order) {
     res
 }
 
-next_knapsack <- function(kss, take) {
-    if (kss$items("size") <= 0L) return(NULL)
-    current_item <- kss$items("top")
-
-    if (take) return(structure(list(
-        capacity = kss$capacity - current_item$weight,
-        items = kss$items("without_top"),
-        value = kss$value + current_item$value,
-        taken = push(kss$taken, current_item)
-    ), class = "knapsack"))
-
-    structure(list(capacity = kss$capacity,
-                   items = kss$items("without_top"),
-                   value = kss$value,
-                   taken = kss$taken),
-              class = "knapsack")
-}
-
-take <- function(ks) next_knapsack(ks, take = TRUE)
-dont_take <- function(ks) next_knapsack(ks, take = FALSE)
-
-sub_problems <- function(ks) {
-    remaining_items <- ks$items
-    if (remaining_items("size") <= 0L) return(list())
-
-    remaining_capacity <- ks$capacity
-    next_item <- remaining_items("top")
-    if (next_item$weight <= remaining_capacity) return(
-        list(next_knapsack(ks, take = TRUE),
-             next_knapsack(ks, take = FALSE))
-    )
-    list(next_knapsack(ks, take = FALSE))
-}
-
-item_df <- function(ks) {
-    is <- ks$items
-    id <- vector("integer", is("size"))
-    weight <- vector("integer", is("size"))
-    value <- vector("integer", is("size"))
-
-    row <- 1L
-    while (is("size") > 0) {
-        item <- is("top")
-        id[row] <- item$id
-        weight[row] <- item$weight
-        value[row] <- item$value
-        is <- is("without_top")
-        row <- row + 1L
-    }
-
-    structure(
-        data.frame(id = id, weight = weight, value = value),
-        class = c("tbl_df", "tbl", "data.frame")
-    )
-}
